a65feddcf064c7cec72bd27b5fdb6e8eee81b9b8
Darrow Palast <dpalast@gmail.com>
2019-07-26 17:11:41 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,12 +1,12 @@
-# def square_array(array)
-#   i = 0
-#   array.each do 
-#     squared_ele = array[i] ** 2
-#     array[i] = squared_ele
-#     i += 1
-#   end
-#   array
-# end
+def square_array(array)
+  i = 0
+  array.each do 
+    squared_ele = array[i] ** 2
+    array[i] = squared_ele
+    i += 1
+  end
+  array
+end
 
 def square_array(array)
   array.collect { |number| number**2 }
